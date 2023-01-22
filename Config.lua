0d539e1249e9ba5f6aac5961191ff90fb1ba964d
Angel <106703423+uwuanangel@users.noreply.github.com>
2023-01-22 08:20:37 +0000

    Bug Fixes and Optimised for Gum Pets and Stables

--- Config.lua
+++ Config.lua
@@ -1,13 +1,22 @@
 Config = {}
 
-Config.Vorp = false
-Config.GumCore = true
+Config.Vorp = true
+Config.GumCore = false
 
-Config.Job = "vet"
 Key = 0x760A9C6F -- G
 
+Config.Job = "horsetrainer" -- set job
+Config.price = 5 -- price of supplies
+Config.amount = 5 -- amount you receive
+
+-- THIS IS OPTIMISED FOR GUM_PETS AND GUM_STABLES ITEMS
+Config.GoldElixir =  "Gold_For_Horse"
+Config.HorseHealth = "Heal_For_Horse"
+Config.HorseStamina = "Boost_For_Horse"
+Config.PetHealth = "Health_For_Dog"
 
 
+-- lOCATIONS WHERE YOU WANT THE VET MENU
 Config.ZonesVet = {
     vetval = {
         Pos = { x = -255.49, y = 741.47, z = 118.26 },
@@ -19,24 +28,4 @@ Config.ZonesVet = {
         Pos = { x = -791.52, y = -1347.83, z = 43.86 },
     },
     
-}
-
-Config.Language = {
-    -- MENU JOB VET---
-    [1] = {text = "Horse Medicine"}, -- label
-    [2] = {text = "Horse Medicine $5"}, -- desc
-    [3] = {text = "Dog Medicine"}, -- label
-    [4] = {text = "Dog Medicine $5"}, -- desc
-    [5] = {text = "Vet Shop"}, -- title
-    [6] = {text = "Buy Vet Supplies"}, -- subtext
-    -- MSG ---
-    [7] = {text = "Press G To Access The Vet Shop"}, -- subtext
-    --- NOTIFY ---
-    [50] = {text = "Vet"},
-    [51] = {text = "You Bought 1 Horse Medicine"},
-    [52] = {text = "You Bought 1 Dog Medicine"},
-    [53] = {text = "Inventory"},
-    [54] = {text = "Vet"},
-    ---BLIP---
-    [100] = {text = "Veterinary"},
 }
