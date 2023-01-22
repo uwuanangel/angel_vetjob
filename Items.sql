aacf549d3f3769c6964066919e4a295a31bb61d9
Angel <106703423+uwuanangel@users.noreply.github.com>
2023-01-22 08:25:46 +0000

    Optimised Items for Gum Pets and Stables

--- Items.sql
+++ Items.sql
@@ -1,7 +1,11 @@
 -- VORP
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('Health_For_Dog', 'Pet Medicine', 20, 1, 'item_standard', 1);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('stim', 'Horse Stimulant', 20, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('Health_For_Dog', 'Pet Bandages', 20, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('Gold_For_Horse', 'Gold Horse Elixir', 20, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('Heal_For_Horse', 'Horse Medicine', 20, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('Boost_For_Horse', 'Horse Stamina Boost', 20, 1, 'item_standard', 1);
 
 -- GUM CORE 
 INSERT INTO `items` VALUES ('Health_For_Dog', 'Pet Medicine', 0.1, 1, 'item_standard', 1, NULL);
-INSERT INTO `items` VALUES ('stim', 'Horse Stimulant', 0.2, 1, 'item_standard', 1, NULL);
+INSERT INTO `items` VALUES ('Gold_For_Horse', 'Horse Gold Elixir', 0.2, 1, 'item_standard', 1, NULL);
+INSERT INTO `items` VALUES ('Heal_For_Horse', 'Horse Medicine', 0.2, 1, 'item_standard', 1, NULL);
+INSERT INTO `items` VALUES ('Boost_For_Horse', 'Horse Boost Elixir', 0.2, 1, 'item_standard', 1, NULL);
